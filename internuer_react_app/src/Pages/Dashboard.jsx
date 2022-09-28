91b054cc714ceac7af47b1af4ba28fe498c6bdd7
idreesahmed1257 <idreesahmef1257@gmail.com>
2022-09-28 17:31:27 +0500

    setting up

---
+++ internuer_react_app/src/Pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Sidebar />
+
+
+        </div>
+    )
+}
+
+export default Dashboard
