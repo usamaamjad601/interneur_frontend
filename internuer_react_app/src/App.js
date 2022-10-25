34d4d5b22fd2569cfc52772934099c78c99f6762
idreesahmed1257 <idreesahmef1257@gmail.com>
2022-10-25 14:20:17 +0500

    UI last

--- internuer_react_app/src/App.js
+++ internuer_react_app/src/App.js
@@ -7,6 +7,10 @@ import DashShortlisted from './Pages/DashShortlisted';
 import DashSubmited from './Pages/DashSubmited';
 import './css/Styles.css'
 import JobLive from './Pages/JobLive';
+import JobDisapproved from './Pages/JobDisapproved';
+import JobInReview from './Pages/JobInReview';
+import JobClosed from './Pages/JobClosed';
+import JobPost from './Pages/JobPost';
 
 
 
@@ -19,7 +23,10 @@ function App() {
         <Route path="DashShortlisted" element={<DashShortlisted />} />
         <Route path='DashSubmited' element={<DashSubmited />} />
         <Route path='joblive' element={<JobLive />} />
-
+        <Route path='jobDisapproved' element={<JobDisapproved />} />
+        <Route path='JobInReview' element={<JobInReview />} />
+        <Route path='JobClosed' element={<JobClosed />} />
+        <Route path='JobPost' element={<JobPost />} />
       </Routes>
     </div>
   );
