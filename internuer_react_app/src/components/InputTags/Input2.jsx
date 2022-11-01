7ce78d2c6239496dc37beb5a669a990b4e3949b6
idreesahmed1257 <idreesahmef1257@gmail.com>
2022-11-01 22:28:20 +0500

    dfd

--- internuer_react_app/src/components/InputTags/Input2.jsx
+++ internuer_react_app/src/components/InputTags/Input2.jsx
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
 import Chip from '@mui/material/Chip';
 import Paper from '@mui/material/Paper';
 import TagFacesIcon from '@mui/icons-material/TagFaces';
+import { ListItem } from '@mui/material';
 
 
 
@@ -11,48 +12,45 @@ import TagFacesIcon from '@mui/icons-material/TagFaces';
 const Input2 = () => {
 
 
-    // const [chipData, setChipData] = React.useState([
-    //     { key: 0, label: 'Angular' },
-    //     { key: 1, label: 'jQuery' },
-    //     { key: 2, label: 'Polymer' },
-    //     { key: 4, label: 'Vue.js' },
-    // ]);
+    const [chipData, setChipData] = React.useState([
+    ]);
 
-    // const handleDelete = (chipToDelete) => () => {
-    //     setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
-    // };
+    const handleDelete = (chipToDelete) => () => {
+        setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+    };
 
     const PushData = (e) => {
-        // const values = document.getElementById("wage");
-        // var joined = chipData.concat({ key: (chipData.length + 1), label: values.value });
 
-        // chipData.push();
-        // setChipData({ key: chipData.length, label: values.value })
-        // setChipData({ chipData: joined });
+        const values = document.getElementById("wage");
+        // var joined = chipData.concat([{ key: (chipData.length + 1), label: values.value }]);
+
+        //chipData.push();
+        setChipData([...chipData, { key: chipData.length + 1, label: values.value }])
+        //setChipData({ chipData: joined });
 
 
-        // console.log(e.target.value)
-        // console.log(joined, 'chipData');
+        console.log(e.target.value)
+        //console.log(joined, 'chipData');
 
 
     }
 
-    const [names, setNames] = React.useState(['Alice', 'Bob']);
+    // const [names, setNames] = React.useState(['Alice', 'Bob']);
 
-    const handleClick = () => {
-        // 👇️ push to end of state array
-        // ducome
-        setNames(current => [...current, 'Carl']);
+    // const handleClick = () => {
+    //     // 👇️ push to end of state array
+    //     // ducome
+    //     setNames(current => [...current, 'Carl']);
 
-        // 👇️ spread an array into the state array
-        // setNames(current => [...current, ...['Carl', 'Delilah']]);
+    //     // 👇️ spread an array into the state array
+    //     // setNames(current => [...current, ...['Carl', 'Delilah']]);
 
-        // 👇️ push to beginning of state array
-        // setNames(current => ['Zoey', ...current]);
-    };
+    //     // 👇️ push to beginning of state array
+    //     // setNames(current => ['Zoey', ...current]);
+    // };
     return (
         <div>
-            {/* <Paper
+            <Paper
                 sx={{
                     display: 'flex',
                     justifyContent: 'center',
@@ -63,9 +61,9 @@ const Input2 = () => {
                 }}
                 component="ul"
             >
-                {chipData.map((data) => {
+                {chipData && chipData.map((data, index) => {
                     return (
-                        <ListItem key={data.key}>
+                        <ListItem key={index}>
                             <Chip
                                 label={data.label}
                                 onDelete={data.label === 'React' ? undefined : handleDelete(data)}
@@ -74,10 +72,10 @@ const Input2 = () => {
                     );
                 })}
             </Paper>
-            <input type="text" id='wage' />
-            <button onClick={PushData}>Validate</button> */}
+            <input type="text" id='wage' onkeydown="search()" />
+            <button onClick={PushData}>Validate</button>
 
-            <div>
+            {/* <div>
                 <div>
                     <input type="text" id='input1' />
                     <button onClick={handleClick}>Push to state array</button>
@@ -91,7 +89,7 @@ const Input2 = () => {
                         </div>
                     );
                 })}
-            </div>
+            </div> */}
         </div>
 
     )
