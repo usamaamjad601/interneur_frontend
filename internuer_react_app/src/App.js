d6a1b7a6f0939b3f89890873122c4e1866073635
idreesahmed1257 <idreesahmef1257@gmail.com>
2022-11-01 23:19:51 +0500

    dfdf

--- internuer_react_app/src/App.js
+++ internuer_react_app/src/App.js
@@ -1,29 +1,29 @@
 import './bootstrap/css/bootstrap.css'
 import React, { Component } from 'react';
 import { Routes, Route } from "react-router-dom";
-// import Dashboard from './Pages/Dashboard';
-// import DashDeclined from './Pages/DashDeclined';
-// import DashShortlisted from './Pages/DashShortlisted';
-// import DashSubmited from './Pages/DashSubmited';
+import Dashboard from './Pages/Dashboard';
+import DashDeclined from './Pages/DashDeclined';
+import DashShortlisted from './Pages/DashShortlisted';
+import DashSubmited from './Pages/DashSubmited';
 import './css/Styles.css'
-// import JobLive from './Pages/JobLive';
-// import JobDisapproved from './Pages/JobDisapproved';
-// import JobInReview from './Pages/JobInReview';
-// import JobClosed from './Pages/JobClosed';
-// import JobPost from './Pages/JobPost';
+import JobLive from './Pages/JobLive';
+import JobDisapproved from './Pages/JobDisapproved';
+import JobInReview from './Pages/JobInReview';
+import JobClosed from './Pages/JobClosed';
+import JobPost from './Pages/JobPost';
 import EmpForms from './Pages/EmpForms';
-// import ClientReg1 from './Pages/Registration/ClientReg1';
-// import ClientReg2 from './Pages/Registration/ClientReg2';
-// import PostInternship from './Pages/Registration/PostInternship';
+import ClientReg1 from './Pages/Registration/ClientReg1';
+import ClientReg2 from './Pages/Registration/ClientReg2';
+import PostInternship from './Pages/Registration/PostInternship';
 import EmpDash from './Pages/EmpDash';
-// import Input2 from './components/InputTags/Input2';
+import Input2 from './components/InputTags/Input2';
 
 
 function App() {
   return (
     <div>
       <Routes>
-        {/* <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="DashDeclined" element={<DashDeclined />} />
         <Route path="DashShortlisted" element={<DashShortlisted />} />
         <Route path='DashSubmited' element={<DashSubmited />} />
@@ -31,7 +31,7 @@ function App() {
         <Route path='jobDisapproved' element={<JobDisapproved />} />
         <Route path='JobInReview' element={<JobInReview />} />
         <Route path='JobClosed' element={<JobClosed />} />
-        <Route path='JobPost' element={<JobPost />} /> */}
+        <Route path='JobPost' element={<JobPost />} />
         <Route path='EmpForms' element={<EmpForms />} />
         {/* <Route path='ClientReg1' element={<ClientReg1 />} />
         <Route path='ClientReg2' element={<ClientReg2 />} />
